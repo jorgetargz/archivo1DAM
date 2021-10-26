030f65003bf31d7fd3a2492171695b9477310c92
jorgetargz <jorgemartin99@outlook.com>
2021-10-26 11:06:42 +0200

    ahorcado

--- Programacion/Ahorcado/src/main/java/main/Main.java
+++ Programacion/Ahorcado/src/main/java/main/Main.java
@@ -1,7 +1,43 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import com.github.javafaker.Faker;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Faker faker = new Faker(new Locale("es"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Nombres de pokemon");
+        System.out.println("2. Colores");
+        System.out.println("3. Nombres de animales");
+        System.out.println("4. Ingredientes");
+        System.out.println("0. Salir");
+        System.out.println("Elige un tema para el ahorcado: ");
+        int tema = sc.nextInt();
+        String palabra = "";
+        switch (tema) {
+            case 0:
+                System.out.println("Gracias por jugar");
+                break;
+            case 1:
+                palabra = faker.pokemon().name().toUpperCase();
+                break;
+            case 2:
+                palabra = faker.color().name().toUpperCase();
+                break;
+            case 3:
+                palabra = faker.animal().name().toUpperCase();
+                break;
+            case 4:
+                palabra = faker.food().ingredient().toUpperCase();
+
+        }
+        System.out.println(palabra);
+
+        }
     }
-}
+
--- Programacion/Ahorcado/src/main/java/main/Pintar.java
+++ Programacion/Ahorcado/src/main/java/main/Pintar.java
@@ -1,7 +1,81 @@
 package main;
 
 public class Pintar {
-    public void DibujarAhorcado(nivel) {
-
+    public void DibujrAhorcado() {
+        int intentosRestantes = 0;
+        switch (intentosRestantes) {
+            case 0:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 1:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 2:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("   |   |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 3:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("  (|   |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 4:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("  (|)  |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 5:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("  (|)  |");
+                System.out.println("   |   |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 6:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("  (|)  |");
+                System.out.println("   |)  |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+            case 7:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("   0   |");
+                System.out.println("  (|)  |");
+                System.out.println("  (|)  |");
+                System.out.println("       |");
+                System.out.println("=========");
+                break;
+        }
     }
 }
