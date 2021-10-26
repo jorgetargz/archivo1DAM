6909c09d2920017f3eaf34362166bf21ed646d63
jorgetargz <jorgemartin99@outlook.com>
2021-10-26 14:22:19 +0200

    ahorcado

--- Programacion/Ahorcado/src/main/java/main/Main.java
+++ Programacion/Ahorcado/src/main/java/main/Main.java
@@ -16,28 +16,94 @@ public class Main {
         System.out.println("3. Nombres de animales");
         System.out.println("4. Ingredientes");
         System.out.println("0. Salir");
-        System.out.println("Elige un tema para el ahorcado: ");
+        System.out.print("Elige un tema para el ahorcado: ");
         int tema = sc.nextInt();
+        sc.nextLine();
         String palabra = "";
+        StringBuilder mostrar = new StringBuilder();
+        StringBuilder letrasProbadas = new StringBuilder();
+        int intentos = 9;
         switch (tema) {
             case 0:
                 System.out.println("Gracias por jugar");
                 break;
             case 1:
-                palabra = faker.pokemon().name().toUpperCase();
+                palabra = faker.pokemon().name();
                 break;
             case 2:
-                palabra = faker.color().name().toUpperCase();
+                palabra = faker.color().name();
                 break;
             case 3:
-                palabra = faker.animal().name().toUpperCase();
+                palabra = faker.animal().name();
                 break;
             case 4:
-                palabra = faker.food().ingredient().toUpperCase();
-
+                palabra = faker.food().ingredient();
+                break;
+            default:
+                System.out.print("Introduce 1-4, 0 para salir:");
+                tema = sc.nextInt();
         }
+        palabra = palabra.toUpperCase();
+        palabra = "DEDEDDE";
         System.out.println(palabra);
-
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) == ' ') {
+                mostrar.append(" ");
+            } else if (palabra.charAt(i) >= 'A' && palabra.charAt(i) <= 'Z') {
+                mostrar.append("_");
+            }
         }
+        System.out.println(mostrar);
+        Pintar DibujarAhorcado = new Pintar();
+        do {
+            System.out.println("1. Pedir letra.");
+            System.out.println("2. Adivinar el ahorcado.");
+            System.out.println("0. Salir");
+            System.out.print("¿Que quieres hacer?: ");
+            int juego = sc.nextInt();
+            sc.nextLine();
+
+            switch (juego) {
+                case 0:
+                    intentos = 0;
+                    DibujarAhorcado.DibujarAhorcado(intentos);
+                    break;
+                case 1:
+                    System.out.println("Letras probadas");
+                    System.out.println(letrasProbadas);
+                    System.out.print("Introduce una letra: ");
+                    char letra = sc.nextLine().toUpperCase().charAt(0);
+                    letrasProbadas.append(letra).append(" ");
+                    if (palabra.indexOf(letra) >= 0) {
+                        System.out.println("Acertaste, la letra "+letra+" esta");
+                        for (int i = 0; i < palabra.length(); i++) {
+                            i = palabra.indexOf(letra, i);
+                            if (i != -1){
+                                mostrar.replace(palabra.indexOf(letra,i),palabra.indexOf(letra,i)+1,""+letra);
+                            } else {
+                                i = palabra.length();
+                            }
+                        }
+                        System.out.println(mostrar);
+                    } else {
+                        System.out.println("Fallaste, la letra "+letra+" no esta");
+                        if (letrasProbadas.indexOf(""+letra) != -1)
+                        intentos--;
+                        DibujarAhorcado.DibujarAhorcado(intentos);
+                        System.out.println(mostrar);
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    break;
+            }
+
+        } while (intentos > 0 && (mostrar.indexOf("_") >= 0));
+        if (mostrar.indexOf("_") < 0) {
+            intentos = 10;
+            DibujarAhorcado.DibujarAhorcado(intentos);
+        }
+
     }
+}
 
--- Programacion/Ahorcado/src/main/java/main/Pintar.java
+++ Programacion/Ahorcado/src/main/java/main/Pintar.java
@@ -1,10 +1,19 @@
 package main;
 
 public class Pintar {
-    public void DibujrAhorcado() {
-        int intentosRestantes = 0;
-        switch (intentosRestantes) {
-            case 0:
+    public void DibujarAhorcado(int intentos) {
+        switch (intentos) {
+            case 9:
+                System.out.println("   +---+");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("       |");
+                System.out.println("=========");
+                System.out.println("Tienes 9 intentos.");
+                break;
+            case 8:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("       |");
@@ -12,8 +21,9 @@ public class Pintar {
                 System.out.println("       |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 8 intentos.");
                 break;
-            case 1:
+            case 7:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("   0   |");
@@ -21,8 +31,9 @@ public class Pintar {
                 System.out.println("       |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 7 intentos.");
                 break;
-            case 2:
+            case 6:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("   0   |");
@@ -30,8 +41,9 @@ public class Pintar {
                 System.out.println("       |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 6 intentos.");
                 break;
-            case 3:
+            case 5:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("   0   |");
@@ -39,6 +51,7 @@ public class Pintar {
                 System.out.println("       |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 5 intentos.");
                 break;
             case 4:
                 System.out.println("   +---+");
@@ -48,8 +61,9 @@ public class Pintar {
                 System.out.println("       |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 4 intentos.");
                 break;
-            case 5:
+            case 3:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("   0   |");
@@ -57,17 +71,19 @@ public class Pintar {
                 System.out.println("   |   |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 3 intentos.");
                 break;
-            case 6:
+            case 2:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("   0   |");
                 System.out.println("  (|)  |");
-                System.out.println("   |)  |");
+                System.out.println("  (|   |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 2 intentos.");
                 break;
-            case 7:
+            case 1:
                 System.out.println("   +---+");
                 System.out.println("   |   |");
                 System.out.println("   0   |");
@@ -75,6 +91,27 @@ public class Pintar {
                 System.out.println("  (|)  |");
                 System.out.println("       |");
                 System.out.println("=========");
+                System.out.println("Tienes 1 intento.");
+                break;
+            case 0:
+                System.out.println("   +---+");
+                System.out.println("   |   |");
+                System.out.println("  x x  |");
+                System.out.println("  (|)  |");
+                System.out.println("  (|)  |");
+                System.out.println("       |");
+                System.out.println("=========");
+                System.out.println("Game over.");
+                break;
+            case 10:
+                System.out.println("   +---+");
+                System.out.println("       |");
+                System.out.println(" $ $   |");
+                System.out.println("  |    |");
+                System.out.println(" (|)   |");
+                System.out.println(" (|)   |");
+                System.out.println("=========");
+                System.out.println("Enhorabuena has ganado.");
                 break;
         }
     }
