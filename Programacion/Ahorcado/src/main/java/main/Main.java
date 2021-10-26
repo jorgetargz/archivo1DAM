6fb6bd3ff734caf8b3f2dd8486fc66dbda01a6bf
jorgetargz <jorgemartin99@outlook.com>
2021-10-26 19:11:15 +0200

    ahorcado rdy

--- Programacion/Ahorcado/src/main/java/main/Main.java
+++ Programacion/Ahorcado/src/main/java/main/Main.java
@@ -9,27 +9,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Pintar DibujarAhorcado = new Pintar();
         Faker faker = new Faker(new Locale("es"));
         Scanner sc = new Scanner(System.in);
+        StringBuilder mostrar = new StringBuilder();
+        StringBuilder letrasProbadas = new StringBuilder();
+        String palabra;
+        int intentos = 9;
+        int tema;
         System.out.println("1. Nombres de pokemon");
         System.out.println("2. Colores");
         System.out.println("3. Nombres de animales");
         System.out.println("4. Ingredientes");
-        System.out.println("0. Salir");
         System.out.print("Elige un tema para el ahorcado: ");
-        int tema = sc.nextInt();
+        tema = sc.nextInt();
         sc.nextLine();
-        String palabra = "";
-        StringBuilder mostrar = new StringBuilder();
-        StringBuilder letrasProbadas = new StringBuilder();
-        int intentos = 9;
         switch (tema) {
-            case 0:
-                System.out.println("Gracias por jugar");
-                break;
-            case 1:
-                palabra = faker.pokemon().name();
-                break;
             case 2:
                 palabra = faker.color().name();
                 break;
@@ -40,70 +35,71 @@ public class Main {
                 palabra = faker.food().ingredient();
                 break;
             default:
-                System.out.print("Introduce 1-4, 0 para salir:");
-                tema = sc.nextInt();
+                palabra = faker.pokemon().name();
+                break;
         }
         palabra = palabra.toUpperCase();
-        palabra = "DEDEDDE";
-        System.out.println(palabra);
         for (int i = 0; i < palabra.length(); i++) {
-            if (palabra.charAt(i) == ' ') {
-                mostrar.append(" ");
-            } else if (palabra.charAt(i) >= 'A' && palabra.charAt(i) <= 'Z') {
+             if (palabra.charAt(i) >= 'A' && palabra.charAt(i) <= 'Z') {
                 mostrar.append("_");
+            } else {
+                mostrar.append(palabra.charAt(i));
             }
         }
-        System.out.println(mostrar);
-        Pintar DibujarAhorcado = new Pintar();
         do {
+            DibujarAhorcado.DibujarAhorcado(intentos);
+            System.out.println("Ahorcado: ");
+            System.out.println(mostrar);
+            System.out.println();
             System.out.println("1. Pedir letra.");
             System.out.println("2. Adivinar el ahorcado.");
             System.out.println("0. Salir");
             System.out.print("¿Que quieres hacer?: ");
             int juego = sc.nextInt();
             sc.nextLine();
-
             switch (juego) {
                 case 0:
                     intentos = 0;
                     DibujarAhorcado.DibujarAhorcado(intentos);
                     break;
-                case 1:
-                    System.out.println("Letras probadas");
-                    System.out.println(letrasProbadas);
+                default:
+                    if (letrasProbadas.length() > 0) {
+                        System.out.println("Letras probadas");
+                        System.out.println(letrasProbadas);
+                    }
                     System.out.print("Introduce una letra: ");
                     char letra = sc.nextLine().toUpperCase().charAt(0);
                     letrasProbadas.append(letra).append(" ");
                     if (palabra.indexOf(letra) >= 0) {
-                        System.out.println("Acertaste, la letra "+letra+" esta");
+                        System.out.println("Acertaste, la letra " + letra + " esta");
                         for (int i = 0; i < palabra.length(); i++) {
                             i = palabra.indexOf(letra, i);
-                            if (i != -1){
-                                mostrar.replace(palabra.indexOf(letra,i),palabra.indexOf(letra,i)+1,""+letra);
+                            if (i >= 0) {
+                                mostrar.replace(palabra.indexOf(letra, i), palabra.indexOf(letra, i) + 1, "" + letra);
                             } else {
                                 i = palabra.length();
                             }
                         }
-                        System.out.println(mostrar);
                     } else {
-                        System.out.println("Fallaste, la letra "+letra+" no esta");
-                        if (letrasProbadas.indexOf(""+letra) != -1)
+                        System.out.println("Fallaste, la letra " + letra + " no esta");
                         intentos--;
-                        DibujarAhorcado.DibujarAhorcado(intentos);
-                        System.out.println(mostrar);
-                        System.out.println();
                     }
                     break;
                 case 2:
+                    System.out.print("¿Cual es?: ");
+                    String respuesta = sc.nextLine();
+                    if (palabra.equalsIgnoreCase(respuesta)) {
+                        mostrar.replace(0, mostrar.length(), respuesta.toUpperCase());
+                    } else {
+                        System.out.println("Fallaste, no es la respuesta correcta");
+                        intentos--;
+                    }
                     break;
             }
-
         } while (intentos > 0 && (mostrar.indexOf("_") >= 0));
         if (mostrar.indexOf("_") < 0) {
             intentos = 10;
             DibujarAhorcado.DibujarAhorcado(intentos);
         }
-
     }
 }
-
